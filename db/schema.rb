8eb58a64b41c398057548dba363e0f9c3676284c
Lincxx <bossco28@gmail.com>
2022-02-18 12:15:36 -0500

    Add Association between User and Doc

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_18_162537) do
+ActiveRecord::Schema.define(version: 2022_02_18_164233) do
 
   create_table "docs", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
