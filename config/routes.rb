4ccac143647842c9eb131025fbbd2fe4113f5c71
Lincxx <bossco28@gmail.com>
2022-02-18 13:42:28 -0500

    Changed root of app based on signed in or not signed in user

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'welcome#index'
+  # root 'welcome#index'
   resources :docs
+  unauthenticated :user do
+    root "welcome#index"
+  end
+
+  authenticated :user do
+    root 'docs#index', as: :authenticated_root
+  end
 end
